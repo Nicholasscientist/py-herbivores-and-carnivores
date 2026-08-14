35689d4d2a01a9c71d0e6504c62b3b459c2864ec
Mykola <mykolamylenko@gmail.com>
2026-08-14 11:59:18 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ from __future__ import annotations
 class Animal:
     alive: list[Animal] = []
 
-    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
+    def __init__(self, name: str, health: int = 100,
+                 hidden: bool = False) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
@@ -15,8 +16,9 @@ class Animal:
         if (self.health <= 0):
             Animal.alive.remove(self)
 
-    def __repr__(self) -> str:
-        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+    def __str__(self) -> str:
+        return (f"{{Name: {self.name},"
+                f"Health: {self.health}, Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
